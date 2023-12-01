a31f71ea9cbefacf0f95dcde0a49b4346ee0133f
callumahern <callum.ahern@simplybusiness.co.uk>
2023-12-01 12:11:24 +0000

    built ruby version

--- sleep-debt-calculator.rb
+++ sleep-debt-calculator.rb
@@ -18,4 +18,36 @@ class SleepDebtCalculator
       9
     end
   end
+
+  def get_actual_sleep_hours
+    get_sleep_hours('Monday') +
+    get_sleep_hours('Tuesday') +
+    get_sleep_hours('Wednesday') +
+    get_sleep_hours('Thursday') +
+    get_sleep_hours('Friday') +
+    get_sleep_hours('Saturday') +
+    get_sleep_hours('Sunday')
+  end
+
+  def get_ideal_sleep_hours
+    ideal_hours = 8
+    ideal_hours * 7
+  end
+
+  def calculate_sleep_debt
+    actual_sleep_hours = get_actual_sleep_hours
+    ideal_sleep_hours = get_ideal_sleep_hours
+
+    if actual_sleep_hours == ideal_sleep_hours
+      print 'Perfect sleep!'
+    elsif actual_sleep_hours > ideal_sleep_hours
+      print "Uh oh - You have overslept #{(ideal_sleep_hours - actual_sleep_hours)} hours!"
+    elsif 
+      print "Get more sleep dammit! You need #{(ideal_sleep_hours - actual_sleep_hours)} hours more to hit your quota"
+
+    end
+  end
 end
+
+hello = SleepDebtCalculator.new
+hello.calculate_sleep_debt
